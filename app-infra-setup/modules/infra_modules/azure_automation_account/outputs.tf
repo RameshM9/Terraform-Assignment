a138929df60764655b16242018800671cf6ed4c4
Muragani <2342156@cognizant.com>
2026-03-23 16:51:26 +0530

    dsc configuration test

--- app-infra-setup/modules/infra_modules/azure_automation_account/outputs.tf
+++ app-infra-setup/modules/infra_modules/azure_automation_account/outputs.tf
@@ -1,2 +1,5 @@
-output "id"   { value = azurerm_automation_account.this.id }
-output "name" { value = azurerm_automation_account.this.name }
+output "id"   { value = azurerm_automation_account.azautomationaccount.id }
+output "name" { value = azurerm_automation_account.azautomationaccount.name }
+
+output "dsc_configuration_name" { value = azurerm_automation_dsc_configuration.azautodscconfiguration.name }
+output "dsc_configuration_id" { value = azurerm_automation_dsc_configuration.azautodscconfiguration.id }
