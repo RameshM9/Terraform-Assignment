7776a8336eaeb6f971a4726e2d9e2028408087c6
Muragani <2342156@cognizant.com>
2026-03-23 16:49:21 +0530

    dsc configuration test

--- app-infra-setup/modules/infra_modules/azure_automation_account/main_automation_account.tf
+++ app-infra-setup/modules/infra_modules/azure_automation_account/main_automation_account.tf
@@ -1,6 +1,25 @@
-resource "azurerm_automation_account" "this" {
+resource "azurerm_automation_account" "azautomationaccount" {
   name                = var.m_automationaccountname
   location            = var.m_location
-  resource_group_name = var.m_location
+  resource_group_name = var.m_resource_group_name
   sku_name            = var.m_sku_name
+
+   identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Purpose   = "dsc-configuration"
+    ManagedBy = "terraform"
+  }
+
+}
+
+resource "azurerm_automation_dsc_configuration" "azautodscconfiguration" {
+  name                    = "azautodscconfiguration"
+  resource_group_name     = m_resource_group_name
+  automation_account_name = azurerm_automation_account.azautomationaccount.name
+  location                = var.m_location
+  # content_embedded        = filebase64(.//WebsiteNgnix.ps1)
+  content_embedded        = "configuration test {}"
 }
